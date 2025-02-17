3881c25e11ddfa9e07b3924975c34a53fe4f1887
Илья <ilya.panarin14@gmail.com>
2025-02-17 16:39:58 +0500

    Добавлен Header

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './Header.module.css';
+import sushiImage from '../../assets/sushi.jpg';
+import { HeaderCartButton } from './HeaderCartButton';
+
+export function Header(props) {
+   return (
+      <React.Fragment>
+         <header className={styles.header}>
+            <h1>Япона Кухня</h1>
+            <HeaderCartButton />
+         </header>
+         <div className={styles['main-image']}>
+            <img src={sushiImage} alt='Sushi' />
+         </div>
+      </React.Fragment>
+   )
+}
