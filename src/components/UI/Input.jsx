d33920bc2b40502cebd516c03892906934966783
Илья Панарин <ilya.panarin14@gmail.com>
2025-06-01 13:28:13 +0500

    Добавил ref

--- src/components/UI/Input.jsx
+++ src/components/UI/Input.jsx
@@ -1,10 +1,11 @@
+import { forwardRef } from 'react';
 import styles from './Input.module.css';
 
-export function Input(props) {
+export const Input = forwardRef((props, ref) => {
    return (
       <div className={styles.input}>
          <label htmlFor={props.input.id}>{props.label}</label>
-         <input {...props.input}/>
+         <input {...props.input} ref={ref}/>
       </div>
    )
-}
+});
